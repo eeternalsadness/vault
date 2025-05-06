ac493a67c8aebe1113fcb3d24969ad567e3d8776
Bach Nguyen <69bnguyen@gmail.com>
2025-05-06 10:57:53 +0700

    update kvv2 & variable descriptions

--- secret-kvv2.tf
+++ secret-kvv2.tf
@@ -6,3 +6,9 @@ resource "vault_mount" "kvv2" {
   }
   description = "KV version 2 secret engine mount"
 }
+
+resource "vault_kv_secret_backend_v2" "kvv2" {
+  mount                = vault_mount.kvv2.path
+  max_versions         = var.kvv2-max-versions
+  delete_version_after = var.kvv2-delete-version-after-seconds
+}
--- variables.tf
+++ variables.tf
@@ -1,28 +1,47 @@
 # REPO PATHS
 
 variable "repo-path-policy" {
-  type    = string
-  default = "config/policies"
+  description = "The path in the repo where policies are configured"
+  type        = string
+  default     = "config/policies"
 }
 
 variable "repo-path-secret-kv" {
-  type    = string
-  default = "config/secrets/kv"
+  description = "The path in the repo where kv secrets (both v1 and v2) are configured"
+  type        = string
+  default     = "config/secrets/kv"
 }
 
 variable "repo-path-auth-jwt" {
-  type    = string
-  default = "config/auth/jwt"
+  description = "The path in the repo where jwt auth backends are configured"
+  type        = string
+  default     = "config/auth/jwt"
 }
 
 # VAULT PATHS
 
 variable "vault-path-secret-kv" {
-  type    = string
-  default = "kvv1"
+  description = "The path in vault where kv secrets are stored"
+  type        = string
+  default     = "kvv1"
 }
 
 variable "vault-path-secret-kv-v2" {
-  type    = string
-  default = "kvv2"
+  description = "The path in vault where kvv2 secrets are stored"
+  type        = string
+  default     = "kvv2"
+}
+
+# KVV2
+
+variable "kvv2-max-versions" {
+  description = "Max number of versions a kvv2 secret can have"
+  type        = number
+  default     = 5
+}
+
+variable "kvv2-delete-version-after-seconds" {
+  description = "Number of seconds a kvv2 secret version should be retained for before getting deleted"
+  type        = number
+  default     = 0
 }
