b206cb97ecd9686fa8bbf69570d2c819fe851f70
Bach Nguyen <69bnguyen@gmail.com>
2025-05-16 19:19:59 +0700

    update auth jwt terraform config

--- auth-jwt.tf
+++ auth-jwt.tf
@@ -5,35 +5,25 @@ locals {
     => yamldecode(file("${path.module}/${var.repo-path-auth-jwt}/${file_name}"))
   }
 
-  auth-jwt-role-mappings = {
-    for k, v in local.auth-jwt-map : k => merge([
-      for file_name in fileset("${path.module}/${var.repo-path-auth-jwt}/${k}/role-mappings", "*.yaml") :
-      yamldecode(file("${path.module}/${var.repo-path-auth-jwt}/${k}/role-mappings/${file_name}"))
-    ]...)
-  }
+  auth-jwt-roles = merge([
+    for backend_name in keys(local.auth-jwt-map) : {
+      for file_name in fileset("${path.module}/${var.repo-path-auth-jwt}/${backend_name}/roles", "*.yaml") :
+      format("%s/%s", backend_name, yamldecode(file("${path.module}/${var.repo-path-auth-jwt}/${backend_name}/roles/${file_name}")).metadata.name)
+      => {
+        backend_name = backend_name
+        role_config  = yamldecode(file("${path.module}/${var.repo-path-auth-jwt}/${backend_name}/roles/${file_name}"))
+      }
+    }
+  ]...)
 
   auth-jwt-bound-claims = {
-    for k, v in local.auth-jwt-role-mappings : k => {
-      for role in keys(v) : role => {
-        bound_claims = {
-          for bound_claim in keys(v[role].boundClaims) :
-          bound_claim => join(",", v[role].boundClaims[bound_claim])
-        }
+    for k, v in local.auth-jwt-roles : k => {
+      bound_claims = {
+        for bound_claim in keys(v.role_config.spec.boundClaims) :
+        bound_claim => join(",", v.role_config.spec.boundClaims[bound_claim])
       }
     }
   }
-
-  auth-oidc-role-map = merge([
-    for k, v in local.auth-jwt-map : {
-      for role in v.spec.oidc.roles :
-      format("%s/%s", k, role.roleName) => {
-        backend_name  = v.metadata.name
-        redirect_urls = v.spec.oidc.redirectUrls
-        role_map      = role
-      }
-    }
-    if v.spec.type == "oidc"
-  ]...)
 }
 
 data "vault_kv_secret_v2" "jwt" {
@@ -48,34 +38,35 @@ resource "vault_jwt_auth_backend" "jwt" {
   for_each = local.auth-jwt-map
 
   description        = each.value.metadata.description
-  oidc_discovery_url = each.value.spec.type == "oidc" ? each.value.spec.oidc.discoveryUrl : null
-  oidc_client_id     = each.value.spec.type == "oidc" ? jsondecode(data.vault_kv_secret_v2.jwt[each.key].data_json).client_id : null
-  oidc_client_secret = each.value.spec.type == "oidc" ? jsondecode(data.vault_kv_secret_v2.jwt[each.key].data_json).client_secret : null
-  bound_issuer       = try(each.value.spec.type == "oidc" ? each.value.spec.oidc.boundIssuer : null, null)
+  oidc_discovery_url = each.value.spec.discoveryUrl
+  oidc_client_id     = try(jsondecode(data.vault_kv_secret_v2.jwt[each.key].data_json).client_id, null)
+  oidc_client_secret = try(jsondecode(data.vault_kv_secret_v2.jwt[each.key].data_json).client_secret, null)
+  bound_issuer       = try(each.value.spec.boundIssuer, null)
   path               = each.value.spec.mountPath
   type               = each.value.spec.type
   tune {
-    listing_visibility = "unauth"
+    listing_visibility = try(each.value.spec.enableOnWebUi, true) ? "unauth" : "hidden"
     default_lease_ttl  = try(each.value.spec.defaultLeaseTtl, null)
     max_lease_ttl      = try(each.value.spec.maxLeaseTtl, null)
+    token_type         = "default-service"
   }
 }
 
-resource "vault_jwt_auth_backend_role" "oidc" {
-  for_each = local.auth-oidc-role-map
+resource "vault_jwt_auth_backend_role" "jwt" {
+  for_each = local.auth-jwt-roles
 
   backend        = vault_jwt_auth_backend.jwt[each.value.backend_name].path
-  role_name      = each.value.role_map.roleName
-  token_policies = each.value.role_map.tokenPolicies
+  role_name      = each.value.role_config.metadata.name
+  token_policies = each.value.role_config.spec.tokenPolicies
 
-  role_type    = "oidc"
-  oidc_scopes  = each.value.role_map.scopes
-  user_claim   = each.value.role_map.userClaim
-  bound_claims = local.auth-jwt-bound-claims[each.value.backend_name][each.value.role_map.roleName].bound_claims
+  role_type       = local.auth-jwt-map[each.value.backend_name].spec.type
+  oidc_scopes     = try(each.value.role_config.spec.scopes, null)
+  user_claim      = each.value.role_config.spec.userClaim
+  bound_claims    = local.auth-jwt-bound-claims[each.key].bound_claims
+  bound_audiences = try(each.value.role_config.spec.boundAudiences, null)
 
-  allowed_redirect_uris = each.value.redirect_urls
+  allowed_redirect_uris = try(local.auth-jwt-map[each.value.backend_name].spec.redirectUrls, null)
 
-  token_explicit_max_ttl = each.value.role_map.tokenTtlSeconds
-  token_ttl              = each.value.role_map.tokenTtlSeconds
+  token_explicit_max_ttl = try(each.value.role_config.spec.tokenTtlSeconds, null)
+  token_ttl              = try(each.value.role_config.spec.tokenTtlSeconds, null)
 }
-
