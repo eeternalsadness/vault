7e2f2a718563cd4d6c359870f41eb2c0174b0421
Bach Nguyen <69bnguyen@gmail.com>
2025-08-10 17:45:55 +0700

    allow organizing policies in folders

--- policy.tf
+++ policy.tf
@@ -1,7 +1,7 @@
 resource "vault_policy" "policy" {
   for_each = {
-    for file_name in fileset(format("%s/%s", path.module, var.repo-path-policy), "*.hcl") :
-    trimsuffix(file_name, ".hcl") => file(format("%s/%s/%s", path.module, var.repo-path-policy, file_name))
+    for file_name in fileset(format("%s/%s", path.module, var.repo-path-policy), "**/*.hcl") :
+    trimsuffix(basename(file_name), ".hcl") => file(format("%s/%s/%s", path.module, var.repo-path-policy, file_name))
   }
 
   name   = each.key
