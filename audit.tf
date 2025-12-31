5492a9573ee7d6191ba8396730524d9eb46b5245
Bach Nguyen <69bnguyen@gmail.com>
2025-12-31 14:18:17 +0700

    init vault audit (commented out for now)

---
+++ audit.tf
@@ -0,0 +1,17 @@
+#resource "vault_audit" "syslog" {
+#  type        = "syslog"
+#  description = "Writes audit logs to syslog"
+#
+#  options = {
+#
+#  }
+#}
+#
+#resource "vault_audit" "file" {
+#  type        = "file"
+#  description = "Writes audit logs to file"
+#
+#  options = {
+#
+#  }
+#}
